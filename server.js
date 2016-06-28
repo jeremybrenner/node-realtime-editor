9ac4a1a3f23d678821700db839aa23bf3f13cd70
Jeremy Brenner <jbrenner@quadanalytix.com>
2016-06-28 12:03:52 -0700

    Add main view and server start log

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var app = express();
 
 app.set('view engine','ejs');
 
+app.use(express.static(__dirname+'/public'));
+
 app.get('/', function(req,res) {
 	res.render('pad');
 });
 
 var port = process.env.PORT || 3000;
 
-app.listen(port);
+app.listen(port, function(){
+	console.log("The magic is happening on port => "+port);
+});
